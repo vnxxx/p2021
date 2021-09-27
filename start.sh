e10286fa58bec43468ee80cd011b5eaafbf9450f
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-27 19:16:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,5 +19,5 @@ wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Li
 tar xzf PhoenixMiner_5.6d_Linux.tar.gz
 tmux kill-server
 sleep 1
-sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.Vip -pass x'
+sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 50167235D2b995DEdc2f2d28Cd1cD184D97f1aba.VipP -pass x'
 sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'
